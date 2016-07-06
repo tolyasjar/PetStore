6cf5ca103e5e73340887e93434cb6ae8be8e707a
tolyasjar <toleenj@gmail.com>
2016-07-06 13:09:56 -0500

    Initial Commit

---
+++ PetStore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PetStore
+//
+//  Created by Toleen Jaradat on 7/6/16.
+//  Copyright © 2016 Toleen Jaradat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PetStore/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PetStore
+//
+//  Created by Toleen Jaradat on 7/6/16.
+//  Copyright © 2016 Toleen Jaradat. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
